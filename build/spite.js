63769738bab1bdf4c2e10bba38cdc8123eaaa491
sporeball <sporeballdev@gmail.com>
2019-12-17 17:36:27 -0800

    cut browserify

--- build/spite.js
+++ build/spite.js
@@ -1,20 +1,13 @@
 var fs = require("fs");
 var path = require("path");
 
-var filePath = path.join(__dirname, "victus.bundle.min.js");
+var filePath = path.join(__dirname, "victus.min.js");
 
 var file = fs.readFileSync(filePath, {encoding: "utf-8"}, function(){});
 
-file = (file.slice(0, 13) + "window.victus=t()" + file.slice(268, file.length));
-file = file.slice(0, 191) + file.slice(270, file.length);
-file = file.slice(0, 319) + file.slice(392, file.length);
-file = file.slice(0, 105) + "return" + file.slice(191, file.length);
-file = file.slice(0, 181) + file.slice(193, file.length);
-file = file.slice(0, 85) + file.slice(112, file.length);
-file = file.slice(0, 179) + ";" + file.slice(180, file.length);
-file = file.slice(0, file.length - 21) + "}]},[1])(1)}));";
 file = file.replace(/!0/g, "1");
 file = file.replace(/!1/g, "0");
+
 file = "// victus | (c) 2019 sporeball | MIT license\n" + file.slice(0, file.length);
 
 fs.writeFileSync(filePath, file);
--- src/victus.js
+++ src/victus.js
@@ -4,209 +4,210 @@
   MIT license
 */
 
-var canvas, ctx, w, h, color;
+!function() {
+  var canvas, ctx, w, h, color, l, z;
 
-setup = obj => {
-  canvas = document.getElementById(obj.id);
-  ctx = canvas.getContext("2d");
-  
-  w = obj.w;
-  h = obj.h;
-  canvas.width = w;
-  canvas.height = h;
-
-  color = obj.color || "#fff";
-}
-
-// private primitive class
-// most other primitives are derived from this
-class Primitive {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-    this.xv = 0;
-    this.yv = 0;
-  }
-  
-  moveTo(x, y) {
-    this.x = x;
-    this.y = y;
-  }
-  
-  moveBy(x, y) {
-    this.x += x;
-    this.y += y;
-  }
-  
-  hide() {
-    if (this.a != null) {
-      this.a = color;
-    } else {
-      this.a = false;
-    }
-  }
-  
-  show() {
-    if (this.a != null) {
-      this.a = this.col;
-    } else {
-      this.a = true;
-    }
-  }
-  
-  clone() {
-    return c(this);
-  }
-  
-  // private update function
-  u() {
-    this.x += this.xv;
-    this.y += this.yv;
-  }
-}
-
-class Rect extends Primitive {
-  constructor(x, y, w, h, col) {
-    super(x, y);
-    this.w = w;
-    this.h = h;
-    this.col = this.a = col;
-  }
-  
-  draw() {
-    this.u();
-    ctx.fillStyle = this.a;
-    ctx.fillRect(this.x, this.y, this.w, this.h);
-  }
-}
-
-class Ellipse extends Primitive {
-  constructor(x, y, w, h, col) {
-    super(x, y);
-    this.w = w;
-    this.h = h;
-    this.col = this.a = col;
-  }
-  
-  draw() {
-    this.u();
-    ctx.fillStyle = this.a;
-    ctx.beginPath();
-    ctx.ellipse(this.x, this.y, this.w, this.h, 0, 0, 2 * Math.PI);
-    ctx.closePath();
-    ctx.fill();
-  }
-}
-
-class Sprite extends Primitive {
-  constructor(spr, x, y) {
-    super(x, y);
-    this.spr = spr;
-    this.s = true;
+  setup = obj => {
+    canvas = document.getElementById(obj.id);
+    ctx = canvas.getContext("2d");
     
-    // image data
-    this.d = new Image;
-    this.d.src = this.spr;
+    w = obj.w;
+    h = obj.h;
+    canvas.width = w;
+    canvas.height = h;
+
+    color = obj.color || "#fff";
   }
-  
-  draw() {
-    this.u();
-    if (this.s) {
-      ctx.drawImage(this.d, this.x, this.y);
-    } else {
-      ctx.fillRect(this.x, this.y, this.d.width, this.d.height);
+
+  // private primitive class
+  // most other primitives are derived from this
+  class Primitive {
+    constructor(x, y) {
+      this.x = x;
+      this.y = y;
+      this.xv = this.yv = 0;
+    }
+    
+    moveTo(x, y) {
+      this.x = x;
+      this.y = y;
+    }
+    
+    moveBy(x, y) {
+      this.x += x;
+      this.y += y;
+    }
+    
+    hide() {
+      if (this.a != null) {
+        this.a = color;
+      } else {
+        this.a = false;
+      }
+    }
+    
+    show() {
+      if (this.a != null) {
+        this.a = this.col;
+      } else {
+        this.a = true;
+      }
+    }
+    
+    clone() {
+      return c(this);
+    }
+    
+    // private update function
+    u() {
+      this.x += this.xv;
+      this.y += this.yv;
     }
   }
-}
 
-class Text extends Primitive {
-  constructor(str, x, y, size = 16, col = "#000", font = "Arial", align = "left") {
-    super(x, y);
-    this.str = str;
-    this.size = size;
-    this.col = this.a = col;
-    this.font = font;
-    this.align = align;
-  }
-  
-  draw() {
-    ctx.fillStyle = this.a;
-    ctx.font = this.size + "px " + this.font;
-    ctx.textAlign = this.align;
-    ctx.fillText(this.str, this.x, this.y);
-  }
-}
-
-class Sound {
-  constructor(snd, vol, loop=0) {
-    this.snd = snd;
-    this.vol = vol;
+  class Rect extends Primitive {
+    constructor(x, y, w, h, col) {
+      super(x, y);
+      this.w = w;
+      this.h = h;
+      this.col = this.a = col;
+    }
     
-    this.d = new Audio(this.snd);
-    this.d.loop = loop;
+    draw() {
+      this.u();
+      ctx.fillStyle = this.a;
+      ctx.fillRect(this.x, this.y, this.w, this.h);
+    }
   }
 
-  reset() {
-    this.d.load();
+  class Ellipse extends Primitive {
+    constructor(x, y, w, h, col) {
+      super(x, y);
+      this.w = w;
+      this.h = h;
+      this.col = this.a = col;
+    }
+    
+    draw() {
+      this.u();
+      ctx.fillStyle = this.a;
+      ctx.beginPath();
+      ctx.ellipse(this.x, this.y, this.w, this.h, 0, 0, 2 * Math.PI);
+      ctx.closePath();
+      ctx.fill();
+    }
   }
 
-  play() {
-    this.reset();
-    this.d.volume = this.vol;
-    this.d.play();
+  class Sprite extends Primitive {
+    constructor(spr, x, y) {
+      super(x, y);
+      this.spr = spr;
+      this.s = true;
+      
+      // image data
+      this.d = new Image;
+      this.d.src = this.spr;
+    }
+    
+    draw() {
+      this.u();
+      if (this.s) {
+        ctx.drawImage(this.d, this.x, this.y);
+      } else {
+        ctx.fillRect(this.x, this.y, this.d.width, this.d.height);
+      }
+    }
   }
 
-  pause() {
-    this.d.pause();
+  class Text extends Primitive {
+    constructor(str, x, y, size = 16, col = "#000", font = "Arial", align = "left") {
+      super(x, y);
+      this.str = str;
+      this.size = size;
+      this.col = this.a = col;
+      this.font = font;
+      this.align = align;
+    }
+    
+    draw() {
+      ctx.fillStyle = this.a;
+      ctx.font = this.size + "px " + this.font;
+      ctx.textAlign = this.align;
+      ctx.fillText(this.str, this.x, this.y);
+    }
   }
-}
 
-clear = () => {
-  ctx.fillStyle = color;
-  ctx.fillRect(0, 0, w, h);
-}
+  class Sound {
+    constructor(snd, vol, loop=0) {
+      this.snd = snd;
+      this.vol = vol;
+      
+      this.d = new Audio(this.snd);
+      this.d.loop = loop;
+    }
 
-var keys = {
-  LeftArrow: "ArrowLeft",
-  UpArrow: "ArrowUp",
-  RightArrow: "ArrowRight",
-  DownArrow: "ArrowDown",
-}
+    reset() {
+      this.d.load();
+    }
 
-for (let i = 0; i < 26; i++) {
-  let l = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".slice(i, i + 1);
-  keys[l] = "Key" + l;
-}
+    play() {
+      this.reset();
+      this.d.volume = this.vol;
+      this.d.play();
+    }
 
-// private clone function
-// adapted from the clone package, by pvorb
-c = parent => {
-  var child, proto;
+    pause() {
+      this.d.pause();
+    }
+  }
+
+  var keys = {
+    Left: "ArrowLeft",
+    Up: "ArrowUp",
+    Right: "ArrowRight",
+    Down: "ArrowDown",
+  }
   
-  if (typeof parent != 'object') {
-    return parent;
+  // loop over each letter of the alphabet to quickly add the rest of the keys
+  z = 0;
+  do {
+    l = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".slice(z, z + 1);
+    keys[l] = "Key" + l;
+    z++;
   }
-  
-  proto = Object.getPrototypeOf(parent);
-  child = Object.create(proto);
+  while (z < 26);
 
-  for (i in parent) {
-    child[i] = c(parent[i]);
+  // private clone function
+  // adapted from the clone package, by pvorb
+  c = parent => {
+    let child;
+    
+    if (typeof parent != 'object') {
+      return parent;
+    }
+    
+    child = Object.create(Object.getPrototypeOf(parent));
+
+    for (i in parent) {
+      child[i] = c(parent[i]);
+    }
+
+    return child;
   }
 
-  return child;
-}
+  x = () => { return ctx; }
 
-x = () => {
-  return ctx;
-}
-
-exports.setup = setup;
-exports.Rect = Rect;
-exports.Ellipse = Ellipse;
-exports.Sprite = Sprite;
-exports.Text = Text;
-exports.Sound = Sound;
-exports.clear = clear;
-exports.keys = keys;
-exports.x = x;
+  window.victus = {
+    setup: setup,
+    Rect: Rect,
+    Ellipse: Ellipse,
+    Sprite: Sprite,
+    Text: Text,
+    Sound: Sound,
+    clear: () => {
+      ctx.fillStyle = color;
+      ctx.fillRect(0, 0, w, h);
+    },
+    keys: keys
+  };
+}();
