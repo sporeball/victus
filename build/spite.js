dfd7274811b2a6233c900822f7f14359f295e24f
sporeball <sporeballdev@gmail.com>
2020-10-14 12:50:59 -0700

    template literal abuse

--- build/spite.js
+++ build/spite.js
@@ -7,6 +7,7 @@ var file = fs.readFileSync(filePath, {encoding: "utf-8"}, function(){});
 
 file = file.replace(/!0/g, "1");
 file = file.replace(/!1/g, "0");
+file = file.replace('("2d")', '`2d`');
 
 file = "// victus | (c) 2020 sporeball | MIT license\n" + file.slice(0, file.length);
 
